bba9b66e2099444c972213885104eec2791747c3
asc303 <chauhansam94@gmail.com>
2021-12-16 23:20:12 +0530

    [ashutosh] add:Employee Payroll

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,3 +1,5 @@
 create database EmplyeePayroll_Service;
 use EmplyeePayroll_Service;
 
+create table employee_Payrolll( id int IDENTITY(1,1) not null primary key, name varchar(150) not null, salary money not null, start date not null)
+select * from employee_Payrolll
